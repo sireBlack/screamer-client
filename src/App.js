4da05a96e7e98bd40fad87c7da6bbd9cb4721a2b
damilare <dblacktin@gmail.com>
2020-01-10 16:16:11 +0100

    display screams on landing page, display time relative to now, bug fixes, etc

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import {ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Components
@@ -27,6 +27,9 @@ const theme = createMuiTheme({
       dark: '#b22a00',
       contrastText: '#fff'
     }
+  },
+  typography: {
+    useNextVariants: true
   }
 });
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+
+import Scream from '../components/Scream';
 
 class home extends Component {
+    state = {
+        screams: null
+    }
+    componentDidMount(){
+        axios.get('/screams')
+         .then(response => {
+             console.log(response.data)
+             this.setState({
+                 screams: response.data
+             })
+         })
+         .catch(err => console.error(err))
+    }
     render() {
+        let recentScreamsMarkup = this.state.screams ? (
+        this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />) 
+        ) : (<p>Loading...</p>);
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <Grid container spacing={10}>
+                <Grid item sm={8} xs={12}>
+                    {recentScreamsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile...</p>
+                </Grid>
+            </Grid>
         )
     }
 }
