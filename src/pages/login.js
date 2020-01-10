d81a2c8690d06e38463ba58c9299ce96fc205db8
damilare <dblacktin@gmail.com>
2020-01-10 17:11:18 +0100

    login form designed

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,77 @@
 import React, { Component } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
+import PropTypes from 'prop-types'
+import AppIcon from '../images/icon.png'
+
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+const styles = {
+    form: {
+        textAlign: 'center'
+    },
+    image: {
+        margin: '20px auto'
+    },
+    pageTitle: {
+        margin: '10px auto'
+    },
+    textField: {
+        margin: '10px auto'
+    },
+    button: {
+        marginTop: 20, 
+    }
+}
 
 class login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            errors: {}
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('form submitted')
+    }
+    
+    handleChange = (event) => {
+        this.setState({
+            [ event.target.name ]: event.target.value
+        })
+    }
+
     render() {
+        const { classes } = this.props
         return (
-            <div>
-                <h1>Login Page</h1>
-            </div>
+            <Grid container className={classes.form}>
+                <Grid item sm />
+                <Grid item sm>
+                    <img src={AppIcon} alt="Logo" className={classes.image} />
+                    <Typography variant="h3" className={classes.pageTitle} >
+                        Login
+                    </Typography>
+                    <form noValidate onSubmit={this.handleSubmit}>
+                        <TextField id="email" name="email" type="email" label="E-Mail" className={classes.textField} value={this.state.email} onChange={this.handleChange} fullWidth />
+                        <TextField id="password" name="password" type="password" label="Password" className={classes.textField} value={this.state.password} onChange={this.handleChange} fullWidth />
+                        <Button type="submit" variant="contained" color="primary" className={classes.button} >Login</Button>
+                    </form>
+                </Grid>
+                <Grid item sm />
+            </Grid>
         )
     }
 }
 
-export default login
+login.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(login);
